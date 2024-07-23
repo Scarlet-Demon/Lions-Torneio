ff8d50a85fd7e8622e5e6041495973331fb3ad7f
Igor Miotto <igorenrique8810@gmail.com>
2024-07-23 20:56:52 -0300

    Update adicionar.js

--- adicionar.js
+++ adicionar.js
@@ -6,10 +6,10 @@ function adicionarFilmes(callback){
         console.log('Não ha Sessões Disponiveis!')
         callback()
     }else{
-        let nome = prompt('Qual o Nome da Sessão?')
-        let data = prompt('Qual a Data da Sessão?')
-        let horario = prompt('Qual o Horário da Sessão?')
-        let sala = prompt('Qual a Sala da Sessão?')
+        let nome = prompt('Qual o Nome da Sessão? ')
+        let data = prompt('Qual a Data da Sessão? ')
+        let horario = prompt('Qual o Horário da Sessão? ')
+        let sala = prompt('Qual a Sala da Sessão? ')
         sessoes.push({nome,data,horario,sala})
         callback()
     }
