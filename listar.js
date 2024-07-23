5922c74e64753209a37821644664e8da88c96f41
Igor Miotto <igorenrique8810@gmail.com>
2024-07-23 17:33:31 -0300

    Update listar.js

--- listar.js
+++ listar.js
@@ -1,4 +1,17 @@
 function listarConsultas(){
 
+        console.log("=======================================================")
+       
+    consultas.forEach((consultas,index)=> {
+        console.log(`
+            ${index + 1}.
+         |nome: ${consultas.nome[i]}| \n
+         |data: ${consultas.data[i]}| \n
+         |hora: ${consultas.hora[i]}| \n
+         |sala: ${consultas.sala[i]}|`)  
+       
+         console.log("=======================================================")
+    });
+   
 }
 module.exports = {listarConsultas}
