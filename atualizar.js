cc50fc1a6f692ad59baa8a47a063a0feb3499d78
Igor Miotto <igorenrique8810@gmail.com>
2024-07-23 20:53:27 -0300

    Update atualizar.js

--- atualizar.js
+++ atualizar.js
@@ -1,15 +1,15 @@
-function atualizarConsultas(callback){
+function atualizarFilmes(callback){
     let num = prompt('Digite o Número da Sessão que deseja Atualizar: ')
     const index = parseInt(num) - 1
     if (index < 0 || index >= consultas.length) {
         console.log('Sessão Não Encontarda, Tente Novamente')
-        atualizarConsultas(callback)
+        atualizarFilmes(callback)
     } else {
         let nome = prompt('Digite o Novo Nome do Filme: ');
         let data = prompt('Digite o Nova Data: ');
         let hora = prompt('Digite o Nova Hora: ');
         let sala = prompt('Digite o Nova Sala: ');
-        consultas[index] = {nome, data, hora, sala}
+        sessoe[index] = {nome, data, hora, sala}
         console.log('Sessão Atualizada com Sucesso!')
     }
     callback()
