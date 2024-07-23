27ff97f58292589e17c09986f6db7222ee65aafe
Scarlet-Demon <igorenrique8810@gmail.com>
2024-07-23 16:58:01 -0300

    Editado

--- atualizar.js
+++ atualizar.js
@@ -1,4 +1,17 @@
-function atualizarConsultas(){
-
+function atualizarConsultas(callback){
+    let num = prompt('Digite o Número da Sessão que deseja Atualizar: ')
+    const index = parseInt(num) - 1
+    if (index < 0 || index >= consultas.length) {
+        console.log('Sessão Não Encontarda, Tente Novamente')
+        atualizarConsultas(callback)
+    } else {
+        let nome = prompt('Digite o Novo Nome do Filme: ');
+        let data = prompt('Digite o Nova Data: ');
+        let hora = prompt('Digite o Nova Hora: ');
+        let sala = prompt('Digite o Nova Sala: ');
+        consultas[index] = {nome, data, hora, sala}
+        console.log('Sessão Atualizada com Sucesso!')
+    }
+    callback()
 }
 module.exports = {atualizarConsultas}
