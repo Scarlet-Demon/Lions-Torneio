0f4e4b78d1536d0a38417e2006ab4a2aaaa88d0b
Igor Miotto <igorenrique8810@gmail.com>
2024-07-23 21:43:41 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,14 +4,14 @@ const { atualizarFilmes } = require('./atualizar')
 const { cancelarIngressos } = require('./cancelar')
 
 function exibirMenu() {
-    console.log('Bem-Vindo ao Cine Ilumination;\n 1. Listar todas as Sessões;\n 2. Adicionar nova Sessão;\n 3. Atualizar uma Sessão Existente;\n 4. Cancelar uma Sessão;\n 5. Sair.')
-    let opcao = prompt('Qual a opção desejada?')
+    console.log('Bem-Vindo ao Cine Ilumination, Prepare a sua Pipoca e a Crinçada para se Divertirem em novas Aventuras, que os Aguarda!!!;\n 1. Adicionar nova Sessão;\n 2. Listar Todas as Sessões;\n 3. Atualizar uma Sessão Existente;\n 4. Cancelar uma Sessão;\n 5. Sair.')
+    let opcao = prompt('Qual á sua Escolha de Hoje: ')
     switch (opcao) {
         case '1':
-            listarFilmes(exibirMenu)
+            adicionarFilmes(exibirMenu)
             break;
         case '2':
-            adicionarFilmes(exibirMenu)
+            listarFilmes(exibirMenu)
             break;
         case '3':
             atualizarFilmes(exibirMenu)
