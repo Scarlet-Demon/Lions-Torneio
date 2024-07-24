316a68d7e45ded170149ad5276bff1c20d6a7688
Igor Miotto <igorenrique8810@gmail.com>
2024-07-23 21:38:02 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,32 +1,32 @@
-const {adicionarFilmes} = require('./adicionar')
-const {listarFilmes} = require('./listar')
-const {atualizarFilmes} = require('./atualizar')
-const {cancelarIngressos} = require('./cancelar')
+const { listarFilmes } = require('./listar')
+const { adicionarFilmes } = require('./adicionar')
+const { atualizarFilmes } = require('./atualizar')
+const { cancelarIngressos } = require('./cancelar')
 
 function exibirMenu() {
-    console.log('Bem-Vindo ao Cine Ilumination; Qual a sua Escolha: ;\n 1. Adicionar nova Sessão;\n 2. Listar todas as Sessões;\n 3. Atualizar uma Sessão Existente;\n 4. Cancelar uma Sessão;\n 5. Sair.')
-    let opcao = prompt('Qual a a opção desejada?')
-        switch (opcao) {
-            case '1':
-		listarFilmes(exibirMenu)
-                break;
-            case '2':
-                adicionarFilmes(exibirMenu)
-                break;
-            case '3':
-                atualizarFilmes(exibirMenu)
-                break;
-            case '4':
-                cancelarFilmes(exibirMenu)
-                break;
-            case '5':
-                return
-                break;
-            default:
-                console.log('Opção inválida, tente novamente.')
-				exibirMenu()
-                break;
-        }
-};
+    console.log('Bem-Vindo ao Cine Ilumination;\n 1. Listar todas as Sessões;\n 2. Adicionar nova Sessão;\n 3. Atualizar uma Sessão Existente;\n 4. Cancelar uma Sessão;\n 5. Sair.')
+    let opcao = prompt('Qual a opção desejada?')
+    switch (opcao) {
+        case '1':
+            listarFilmes(exibirMenu)
+            break;
+        case '2':
+            adicionarFilmes(exibirMenu)
+            break;
+        case '3':
+            atualizarFilmes(exibirMenu)
+            break;
+        case '4':
+            cancelarIngressos(exibirMenu)
+            break;
+        case '5':
+            return
+            break;
+        default:
+            console.log('Opção inválida, tente novamente.')
+            exibirMenu()
+            break;
+    }
+}
 
 module.exports = {exibirMenu}
