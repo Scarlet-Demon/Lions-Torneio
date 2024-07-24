bd1288303bc13e927c7bb7aaaa51ba2714e44bdf
Igor Miotto <igorenrique8810@gmail.com>
2024-07-24 15:13:11 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,7 +4,16 @@ const { atualizarSessao } = require('./atualizar')
 const { cancelarIngressos } = require('./cancelar')
 
 function exibirMenu() {
-    console.log('Bem-Vindo ao Cine Ilumination, Prepare a sua Pipoca e a Crinçada para se Divertirem em novas Aventuras, que os Aguarda!!!;\n 1. Adicionar nova Sessão;\n 2. Listar Todas as Sessões;\n 3. Atualizar uma Sessão Existente;\n 4. Cancelar uma Sessão;\n 5. Sair.')
+    console.log(`
+    +==============================================================================================================================+
+    |   Bem-Vindo ao Cine Ilumination, Prepare a sua Pipoca e a Crinçada para se Divertirem em novas Aventuras, que os Aguarda!!!; |
+    |       1. Adicionar nova Sessão;                                                                                              |
+    |       2. Listar Todas as Sessões;                                                                                            |
+    |       3. Atualizar uma Sessão Existente;                                                                                     |
+    |       4. Cancelar uma Sessão;                                                                                                |
+    |       5. Sair.                                                                                                               |
+    +==============================================================================================================================+
+    `)                                                                                                             |
     let opcao = prompt('Qual á sua Escolha de Hoje: ')
     switch (opcao) {
         case '1':
@@ -21,7 +30,13 @@ function exibirMenu() {
             cancelarIngressos(exibirMenu)
             break;
         case '5':
-            console.log(`Eaí se divertiu Amiguinho(a)?, É uma pena que você está indo embora, mais isso não é um Adeus, e sim um Até Logo, pois o Mundo é Cheio de Aventuras, que está sempre buscando um novo Protagonista. Espero que um dia ele seja Você e que eu possa contar á sua História. Até logo Amiguinho(a), te vejo por ai em meio às Histórias do Mundo.`)
+            console.log(`
+            +================================================================================================================================+
+            | Eaí se divertiu Amiguinho(a)?, É uma pena que você está indo embora, mais isso não é um Adeus, e sim um Até Logo, pois o Mundo |
+            | é Cheio de Aventuras, que está sempre buscando um novo Protagonista. Espero que um dia ele seja Você e que eu possa contar á   |
+            | sua História. Até logo Amiguinho(a), te vejo por ai em meio às Histórias do Mundo.                                             |
+            +================================================================================================================================+
+            `)
             return
             break;
         default:
